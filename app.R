7e27295ea7ca017c43eb714a145d010f24dbc2d7
Nicholas Chen <nicholaschen314@gmail.com>
2023-11-20 13:16:44 -0500

    finalized inputs

--- app.R
+++ app.R
@@ -1,6 +1,13 @@
 library(shiny)
 library(tidyverse)
+library(ggplot2)
 library(bslib)
+library(scales)
+library(DT)
+library(thematic)
+library(shinyWidgets)
+library(plotly)
+thematic_shiny(font = "auto")
 
 ##Business Logic
 
@@ -11,7 +18,14 @@ grants_prime <- read_csv("./data-raw/Assistance_PrimeAwardSummaries_2023-10-09_H
 source("./.R/func_clean_df.R")
 joined_df <- func_clean_df(grants_prime, contracts_prime)
 
+##need to fully clean country names
+##need to join df with recipients df 
 
+##defines min and max FY and award thresholds
+year_min <- min(joined_df$award_base_action_date_fiscal_year)
+year_max <- max(joined_df$award_base_action_date_fiscal_year)
+award_min <- min(joined_df$total_obligated_amount)
+award_max <- max(joined_df$total_obligated_amount)
 
 # Define UI for application that shows data
 ui <- fluidPage(
@@ -23,23 +37,37 @@ ui <- fluidPage(
   ),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("number", "select a number", 0, 100, 40),
-      selectInput("countryvar", "Select a Country Code",
-                  choices = c(unique(joined_df$recipient_country_code))),
-      checkboxGroupInput("years", "Select Award Years:", 
-                         choices = unique(joined_df$award_base_action_date_fiscal_year), 
-                         selected = 2022, inline = TRUE),
-      selectInput("grantvar", "Select a Type", choices = c("Grant", "Local")),
-      numericInput("awardvar", "Awarded Money:", 
-                   min = 0, max = max(joined_df$total_obligated_amount), 
-                   value = 0)
-      
-      
-      
+      selectizeInput("ppp_country1", "Search a Country",
+                  choices = c(unique(joined_df$primary_place_of_performance_country_name)),
+                  options = list(placeholder = "select a country name")),
+      sliderTextInput(
+        inputId = "year1",
+        label = "Year Select",
+        choices = c(year_min:year_max),
+        selected =c(year_min, year_max),
+        grid = FALSE, dragRange = FALSE),
+      searchInput(
+        inputId = "awardthreshold", 
+        label = "Enter Minimum Award Value($)", 
+        placeholder = "Enter Integer", 
+        btnSearch = icon("search"), 
+        btnReset = icon("remove"),
+        resetValue = 0, 
+        width = "100%"
+      ),
+      pickerInput("award_type1", "What type(s) of awards?",
+                  choices = c("Grant", "Contract"),
+                  selected = c("Grant", "Contract"),
+                  multiple = TRUE, 
+                  options = pickerOptions(
+                    actionsBox = TRUE, 
+                    size = 10
+                  )
+      )
     ),
     mainPanel(
       tabsetPanel(
-        tabPanel("a",plotOutput("hist")),
+        tabPanel("a"),
         tabPanel("b"),
         tabPanel("c")
       )
@@ -49,13 +77,6 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-  output$hist <- renderPlot({
-    
-    ggplot(data = joined_df, mapping = aes(x = recipient_country_code, 
-                                           y = total_obligated_amount, 
-                                           fill = is.local)) +
-      geom_histogram()
-  })
 }
 
 # Run the application 
